27da034edf6071b95bf3559b3f97cf663d3bce58
Jan Müller <janmueller3698@gmail.com>
2020-12-09 09:14:25 +0100

    Project configuration

--- karma.conf.js
+++ karma.conf.js
@@ -36,9 +36,15 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    autoWatch: false,
+    browsers: ['ChromeForAutomation'],
+    customLaunchers: {
+      ChromeForAutomation: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions', '--no-sandbox', '--disable-gpu']
+      }
+    },
+    singleRun: true,
+    restartOnFileChange: false
   });
 };
