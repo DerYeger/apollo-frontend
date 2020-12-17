89e88cff63a3572a4f1195c8b9faf1604be81aff
Jan Müller <janmueller3698@gmail.com>
2020-12-17 22:24:09 +0100

    SnackBar: Implemented undoing of graph deletions

--- src/app/services/snack-bar.service.ts
+++ src/app/services/snack-bar.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarRef, TextOnlySnackBar } from '@angular/material/snack-bar';
+import { Store } from '@ngrx/store';
 import { TranslateService } from '@ngx-translate/core';
 import { NGXLogger } from 'ngx-logger';
 import { forkJoin, Observable, of } from 'rxjs';
 import { FOLGraph } from '../model/domain/fol.graph';
+import { storeGraph } from '../store/actions';
+import { State } from '../store/state';
 
 export interface TranslationDTO {
   key: string;
@@ -14,20 +17,22 @@ export interface TranslationDTO {
   providedIn: 'root',
 })
 export class SnackBarService {
-  constructor(private readonly snackBar: MatSnackBar, private readonly translate: TranslateService, private readonly log: NGXLogger) {}
+  constructor(private readonly snackBar: MatSnackBar, private readonly translate: TranslateService, private readonly store: Store<State>, private readonly log: NGXLogger) {}
 
-  public openSnackBar(messageDTO: TranslationDTO, actionDTO?: TranslationDTO, duration: number | undefined = 2000): void {
+  public async openSnackBar(messageDTO: TranslationDTO, actionDTO?: TranslationDTO, duration: number | undefined = 2000): Promise<MatSnackBarRef<TextOnlySnackBar>> {
     const message$: Observable<string> = this.translate.get(messageDTO.key, messageDTO.params);
     const action$: Observable<string | undefined> = actionDTO ? this.translate.get(actionDTO.key, actionDTO.params) : of(undefined);
-    forkJoin([message$, action$])
-      .toPromise()
-      .then(([message, action]) => {
-        this.log.debug(`Openend SnackBar - Message: ${message} - Action: ${action}`);
-        this.snackBar.open(message, action, { duration });
-      });
+    const [message, action] = await forkJoin([message$, action$]).toPromise();
+    this.log.debug(`Openend SnackBar - Message: ${message} - Action: ${action}`);
+    return this.snackBar.open(message, action, { duration });
   }
 
   public graphDeleted(graph: FOLGraph): void {
-    this.openSnackBar({ key: 'snackbar.graph-deleted', params: { name: graph.name } }, undefined, 2000);
+    this.openSnackBar({ key: 'snackbar.graph-deleted', params: { name: graph.name } }, { key: 'actions.undo' }, 10000).then((snackBarRef) =>
+      snackBarRef
+        .onAction()
+        .toPromise()
+        .then(() => this.store.dispatch(storeGraph(graph)))
+    );
   }
 }
