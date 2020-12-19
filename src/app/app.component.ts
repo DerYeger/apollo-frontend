2c993ff693c660107d1e5f3173637313621a4235
Jan Müller <janmueller3698@gmail.com>
2020-12-19 19:37:25 +0100

    Project: Fixed theme not applying to some elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
-import localeDe from '@angular/common/locales/en';
-import localeEn from '@angular/common/locales/en';
-import { TranslateService } from '@ngx-translate/core';
-import { NGXLogger } from 'ngx-logger';
 import { registerLocaleData } from '@angular/common';
-import { State } from './store/state';
+import localeDe from '@angular/common/locales/de';
+import localeEn from '@angular/common/locales/en';
+import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
+import * as d3 from 'd3';
+import { NGXLogger } from 'ngx-logger';
 import { Subscription } from 'rxjs';
-import { setLanguage } from './store/actions';
 import { map } from 'rxjs/operators';
-import { runInThisContext } from 'vm';
+
+import { State, Theme } from './store/state';
 
 @Component({
   selector: 'gramofo-root',
@@ -24,8 +24,6 @@ export class AppComponent implements OnDestroy, OnInit {
 
   private languageSubscription?: Subscription;
   private themeSubscription?: Subscription;
-
-  @HostBinding('class') public themeClass!: string;
 
   constructor(private readonly store: Store<State>, private readonly translate: TranslateService, private readonly log: NGXLogger) {
     Object.entries(this.languages).forEach(([language, locale]) => {
@@ -45,9 +43,12 @@ export class AppComponent implements OnDestroy, OnInit {
     this.themeSubscription = this.store
       .select('settings')
       .pipe(map((settings) => settings.theme))
-      .subscribe((theme) => {
-        this.themeClass = theme;
-      });
+      .subscribe((theme) =>
+        d3
+          .select(document.body)
+          .classed('dark-theme', 'dark-theme' === theme)
+          .classed('light-theme', 'light-theme' === theme)
+      );
   }
 
   ngOnDestroy(): void {
