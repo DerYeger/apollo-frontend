d664fb110cb410702444bfe9971966aafe8ae3f8
Jan Müller <janmueller3698@gmail.com>
2020-12-21 10:04:50 +0100

    Graph: Improved bidirectional links

--- src/app/utils/d3.utils.ts
+++ src/app/utils/d3.utils.ts
@@ -18,15 +18,21 @@ export function paddedLinePath(source: D3Node, target: D3Node, graphConfiguratio
 }
 
 export function paddedArcPath(source: D3Node, target: D3Node, graphConfiguration: GraphConfiguration): string {
-  const deltaX = target.x! - source.x!;
-  const deltaY = target.y! - source.y!;
-  const dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-  const normX = deltaX / dist;
-  const normY = deltaY / dist;
-  const targetX = target.x! - (graphConfiguration.markerPadding - 1) * normX;
-  const targetY = target.y! - (graphConfiguration.markerPadding - 1) * normY;
-  return `M${source.x},${source.y}
-          A${dist},${dist},0,0,1,${targetX},${targetY}`;
+  const s = new Matrix([[source.x!, source.y!]]);
+  const t = new Matrix([[target.x!, target.y!]]);
+  const diff = Matrix.subtract(t, s);
+  const dist = diff.norm('frobenius');
+  const norm = diff.divide(dist);
+  const rotation = degreesToRadians(10);
+  const start = rotate(norm, -rotation).multiply(graphConfiguration.nodeRadius).add(s);
+  const endNorm = Matrix.multiply(norm, -1);
+  const end = rotate(endNorm, rotation)
+    .multiply(graphConfiguration.nodeRadius)
+    .add(t)
+    .add(rotate(endNorm, rotation).multiply(2 * graphConfiguration.markerBoxSize));
+  const arcRadius = 1.2 * dist;
+  return `M${start.get(0, 0)},${start.get(0, 1)}
+          A${arcRadius},${arcRadius},0,0,1,${end.get(0, 0)},${end.get(0, 1)}`;
 }
 
 export function paddedReflexivePath(node: D3Node, center: [number, number], graphConfiguration: GraphConfiguration): string {
@@ -37,7 +43,7 @@ export function paddedReflexivePath(node: D3Node, center: [number, number], grap
   }
   const diff = Matrix.subtract(n, c);
   const norm = diff.divide(diff.norm('frobenius'));
-  const rotation = 40 * (Math.PI / 180);
+  const rotation = degreesToRadians(40);
   const start = rotate(norm, rotation).multiply(graphConfiguration.nodeRadius).add(n);
   const end = rotate(norm, -rotation)
     .multiply(graphConfiguration.nodeRadius)
@@ -45,12 +51,6 @@ export function paddedReflexivePath(node: D3Node, center: [number, number], grap
     .add(rotate(norm, -rotation).multiply(2 * graphConfiguration.markerBoxSize));
   return `M${start.get(0, 0)},${start.get(0, 1)}
           A${graphConfiguration.nodeRadius},${graphConfiguration.nodeRadius},0,1,0,${end.get(0, 0)},${end.get(0, 1)}`;
-}
-
-function rotate(vector: Matrix, radians: number): Matrix {
-  const x = vector.get(0, 0);
-  const y = vector.get(0, 1);
-  return new Matrix([[x * Math.cos(radians) - y * Math.sin(radians), x * Math.sin(radians) + y * Math.cos(radians)]]);
 }
 
 export function linePath(from: [number, number], to: [number, number]): string {
@@ -65,10 +65,16 @@ export function directLinkTextTransform(source: D3Node, target: D3Node): string 
 }
 
 export function bidirectionalLinkTextTransform(source: D3Node, target: D3Node, graphConfiguration: GraphConfiguration): string {
-  const angle = Math.atan2(source.y! - target.y!, source.x! - target.x!);
-  const xOffset = 2 * graphConfiguration.nodeRadius * Math.cos(angle) + target.x!;
-  const yOffset = 2 * graphConfiguration.nodeRadius * Math.sin(angle) + target.y!;
-  return `translate(${xOffset},${yOffset})`;
+  const s = new Matrix([[source.x!, source.y!]]);
+  const t = new Matrix([[target.x!, target.y!]]);
+  const diff = Matrix.subtract(t, s);
+  const dist = diff.norm('frobenius');
+  const norm = diff.divide(dist);
+  const rotation = degreesToRadians(10);
+  const endNorm = Matrix.multiply(norm, -1);
+  const translation = 0.3 * dist > 2 * graphConfiguration.nodeRadius ? 0.3 * dist : 2 * graphConfiguration.nodeRadius;
+  const end = rotate(endNorm, rotation).multiply(translation).add(t);
+  return `translate(${end.get(0, 0)},${end.get(0, 1)})`;
 }
 
 export function reflexiveLinkTextTransform(node: D3Node, center: [number, number], graphConfiguration: GraphConfiguration): string {
@@ -83,4 +89,14 @@ export function reflexiveLinkTextTransform(node: D3Node, center: [number, number
     .multiply(3 * graphConfiguration.nodeRadius + 8)
     .add(n);
   return `translate(${offset.get(0, 0)},${offset.get(0, 1)})`;
+}
+
+function degreesToRadians(degrees: number): number {
+  return degrees * (Math.PI / 180);
+}
+
+function rotate(vector: Matrix, radians: number): Matrix {
+  const x = vector.get(0, 0);
+  const y = vector.get(0, 1);
+  return new Matrix([[x * Math.cos(radians) - y * Math.sin(radians), x * Math.sin(radians) + y * Math.cos(radians)]]);
 }
