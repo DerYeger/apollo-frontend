fb5bba05270c102cc70d901d2aca1172b0b0ba52
Jan Müller <janmueller3698@gmail.com>
2021-01-06 15:51:56 +0100

    ResultTree: Added filter for causes

--- src/app/components/dialogs/result-tree/result-tree.dialog.ts
+++ src/app/components/dialogs/result-tree/result-tree.dialog.ts
@@ -4,43 +4,62 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { ModelCheckerTrace } from 'src/app/model/api/model-checker-trace';
 
+interface TraceNode {
+  trace: ModelCheckerTrace;
+  children: TraceNode[];
+  visible: boolean;
+}
+
 @Component({
   templateUrl: './result-tree.dialog.html',
   styleUrls: ['./result-tree.dialog.scss'],
 })
 export class ResultTreeDialog {
-  public readonly treeControl = new NestedTreeControl<ModelCheckerTrace>((trace) => trace.children);
-  public readonly dataSource = new MatTreeNestedDataSource<ModelCheckerTrace>();
+  public readonly treeControl = new NestedTreeControl<TraceNode>((node) => node.children);
+  public readonly dataSource = new MatTreeNestedDataSource<TraceNode>();
 
-  constructor(public readonly dialogRef: MatDialogRef<ResultTreeDialog>, @Inject(MAT_DIALOG_DATA) public readonly root: ModelCheckerTrace) {
-    this.dataSource.data = [root];
-    this.treeControl.dataNodes = [root];
+  public readonly rootNode: TraceNode;
+
+  constructor(public readonly dialogRef: MatDialogRef<ResultTreeDialog>, @Inject(MAT_DIALOG_DATA) public readonly rootTrace: ModelCheckerTrace) {
+    this.rootNode = traceToNode(rootTrace);
+    const data = [this.rootNode];
+    this.dataSource.data = data;
+    this.treeControl.dataNodes = data;
+    this.treeControl.expandAll();
   }
 
-  hasChild = (_: number, trace: ModelCheckerTrace) => !!trace.children && trace.children.length > 0;
+  public hasChild = (_: number, node: TraceNode) => !!node.children && node.children.length > 0;
 
-  showCauses(): void {
-    this.treeControl.collapseAll();
-    if (this.root.isModel) {
-      this.expandValidTraces(this.root);
+  public updateFilter(filter: boolean): void {
+    if (!filter) {
+      clearFilter(this.rootNode);
     } else {
-      this.expandInvalidTraces(this.root);
+      filterCauses(this.rootNode, this.rootNode.trace.isModel);
     }
   }
+}
 
-  private expandValidTraces(trace: ModelCheckerTrace): void {
-    if (trace.isModel !== trace.shouldBeModel) {
-      return;
-    }
-    this.treeControl.expand(trace);
-    trace.children.forEach((child) => this.expandValidTraces(child));
-  }
+function clearFilter(node: TraceNode): void {
+  node.visible = true;
+  node.children.forEach((child) => clearFilter(child));
+}
 
-  private expandInvalidTraces(trace: ModelCheckerTrace): void {
-    if (trace.isModel === trace.shouldBeModel) {
-      return;
-    }
-    this.treeControl.expand(trace);
-    trace.children.forEach((child) => this.expandInvalidTraces(child));
+function filterCauses(node: TraceNode, expected: boolean): void {
+  // Check if trace behaved as required.
+  const actual = node.trace.isModel === node.trace.shouldBeModel;
+  // Check if filtering is for positive or negative causes
+  if (actual === expected) {
+    node.visible = true;
+    node.children.forEach((child) => filterCauses(child, expected));
+  } else {
+    node.visible = false;
   }
+}
+
+function traceToNode(trace: ModelCheckerTrace): TraceNode {
+  return {
+    trace,
+    children: trace.children.map((child) => traceToNode(child)),
+    visible: true,
+  };
 }
